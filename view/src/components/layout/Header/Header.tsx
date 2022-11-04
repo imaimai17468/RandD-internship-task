8e9313e7d2a83b5950ea290f73602a0eb96726b7
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-05 01:07:53 +0900

    fix: Headerをlayoutからimportできるようにする

--- view/src/components/layout/Header/Header.tsx
+++ view/src/components/layout/Header/Header.tsx
@@ -1,11 +1,15 @@
-export const Header = () => {
+import Link from 'next/link'
+
+export default function Header(): JSX.Element {
   return (
     <div className="h-16 w-full bg-primary-1">
-      <div className="h-full mx-5 flex flex-row align-center items-center my-auto p-0 gap-10">
-        <p className=" text-3xl text-text-main-white">imaimai chat</p>
+      <div className="align-center mx-5 my-auto flex h-full flex-row items-center gap-10 p-0">
+        <Link href="/">
+          <p className=" text-3xl text-text-main-white">imaimai chat</p>
+        </Link>
         <div className="flex flex-row gap-5 text-xl text-text-main-white">
-          <p>HOME</p>
-          <p>CHAT</p>
+          {/* <p>HOME</p>
+          <p>CHAT</p> */}
         </div>
       </div>
     </div>
