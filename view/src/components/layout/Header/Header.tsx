ba80bb162bd1ce3d56398f45d43a42cea175b798
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-06 00:06:06 +0900

    feat: 現行デザインをレスポンシブデザインにする

--- view/src/components/layout/Header/Header.tsx
+++ view/src/components/layout/Header/Header.tsx
@@ -5,7 +5,7 @@ export default function Header(): JSX.Element {
     <div className="h-16 w-full bg-primary-1">
       <div className="align-center mx-5 my-auto flex h-full flex-row items-center gap-10 p-0">
         <Link href="/">
-          <p className=" text-3xl text-text-main-white">imaimai chat</p>
+          <p className=" lg:text-3xl text-xl text-text-main-white">imaimai chat</p>
         </Link>
         <div className="flex flex-row gap-5 text-xl text-text-main-white">
           {/* <p>HOME</p>
