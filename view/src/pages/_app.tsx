54ae50b5d987343b535a2d2d73a013a7b7b5cee1
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-11 23:32:19 +0900

    [feat] ログイン用のfirebaseのhooksや設定を追加(#28)

--- view/src/pages/_app.tsx
+++ view/src/pages/_app.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+import { AuthProvider } from '@hooks/AuthContext'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
+    <AuthProvider>
+      <RecoilRoot>
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </AuthProvider>
   )
 }
