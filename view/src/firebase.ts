06eb620139868040cb549ded3a5e95ac0badd029
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-11 12:55:05 +0900

    [feat] firebaseの導入(#7)

---
+++ view/src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
+
+if (!firebase.apps.length) {
+  const firebaseApp = firebase.initializeApp({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGE_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  })
+}
+
+export const FBApp = firebase
+export const db = firebase.firestore()
