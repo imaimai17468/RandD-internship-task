939fb1f3599a9f3ed8eab1fff7e194f9bccae9d2
imaimai17468 <tosiki_801_life@outlook.jp>
2022-11-13 23:20:08 +0900

    [feat] ハンバーガーメニューの実装(#37)

--- view/src/components/layout/Header/Header.tsx
+++ view/src/components/layout/Header/Header.tsx
@@ -5,10 +5,11 @@ import { Button } from '@components/common'
 import { signOut } from 'firebase/auth'
 import { auth } from '../../../firebase'
 import Router from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Header(): JSX.Element {
   const [user, setUser] = useRecoilState(userState)
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
 
   const initValue = {
     id: -1,
@@ -22,27 +23,53 @@ export default function Header(): JSX.Element {
     Router.push('/')
   }
 
+  const isNotLoginHeader = (
+    <Link href="/auth">
+      <p className="text-sm text-text-main-white lg:text-xl">
+        ログイン / 新規登録
+      </p>
+    </Link>
+  )
+
+  const openMenuHandler = () => {
+    setIsOpenMenu(!isOpenMenu)
+  }
+
+  const menu = (
+    <div className="absolute top-16 right-5 flex h-20v w-1/3 flex-col items-center justify-center gap-5 rounded-lg bg-white shadow-lg md:w-1/6">
+      {/* ホバーすると影がつく背景が白色のボタン */}
+      <button className="h-1/3 w-3/5 rounded-lg hover:shadow-lg">
+        <p className="mt-2 text-lg text-black">マイページ</p>
+      </button>
+      <Button onClick={handleSignOut}>ログアウト</Button>
+    </div>
+  )
+
+  const isLoginHeader = (
+    <div className="ml-auto flex w-1/2 flex-row items-center justify-center md:w-1/5">
+      <button
+        onClick={openMenuHandler}
+        className="group flex h-full w-full flex-row items-center justify-center gap-3 rounded-lg hover:bg-primary-2"
+      >
+        <p className="text-sm md:text-xl">{user.name}</p>
+        <p className="text-xl text-white transition duration-300 group-hover:rotate-180">
+          🐸
+        </p>
+      </button>
+      {isOpenMenu && menu}
+    </div>
+  )
+
   return (
     <div className="h-16 w-full bg-primary-1">
-      <div className="align-center mx-5 my-auto flex h-full flex-row items-center gap-10 p-0">
+      <div className="align-center mx-5 flex h-full flex-row items-center justify-between gap-10 p-0">
         <Link href="/">
-          <p className=" text-md text-text-main-white lg:text-3xl">
+          <p className=" text-md text-text-main-white md:text-3xl">
             imaimai chat
           </p>
         </Link>
-        <div className="ml-auto flex flex-row gap-5 text-xl text-text-main-white">
-          {user.name == '' ? (
-            <Link href="/auth">
-              <p className="text-sm text-text-main-white lg:text-xl">
-                ログイン / 新規登録
-              </p>
-            </Link>
-          ) : (
-            <div className="flex flex-row items-center gap-3">
-              <Button onClick={handleSignOut}>ログアウト</Button>
-              <p className="text-sm md:text-xl">{user.name}</p>
-            </div>
-          )}
+        <div className="flex h-full flex-1 flex-row gap-5 text-xl text-text-main-white">
+          {user.name == '' ? isNotLoginHeader : isLoginHeader}
         </div>
       </div>
     </div>
